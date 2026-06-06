a58ed096ac298034fb32d1365add697b3596a3cb
Weintag <86998926+Weintag@users.noreply.github.com>
2026-06-06 14:01:20 +0800

    Delete 11_himework_js directory

--- 11_himework_js/js/main.js
+++
@@ -1,9 +0,0 @@
-import { getTodosFromLocalStorage } from "./storage.js";
-import { renderTodos, initTodoHandlers } from "./dom.js";
-
-const todos = getTodosFromLocalStorage() || [];
-
-document.addEventListener("DOMContentLoaded", () => {
-	renderTodos(todos);
-	initTodoHandlers(todos);
-});
--- 11_himework_js/js/storage.js
+++
@@ -1,7 +0,0 @@
-export const getTodosFromLocalStorage = () => {
-	return JSON.parse(localStorage.getItem("todos"));
-};
-
-export const setTodoLocalStorage = todos => {
-	localStorage.setItem("todos", JSON.stringify(todos));
-};
