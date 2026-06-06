5af54447a1c4a45e0ab0b56c80c13a99967e7a42
Weintag <86998926+Weintag@users.noreply.github.com>
2026-06-06 16:17:51 +0800

    JS project commit

---
+++ mobile-gadget-js/js/index.js
@@ -0,0 +1,45 @@
+import HeaderFixed from "./header.js";
+import Modal from "./modal.js";
+import BurgerMenu from "./burger.js";
+import { productSlider } from "./product-slider.js";
+import { dropdownMenu } from "./dropdown.js";
+import { buttonsSearch } from "./buttonsInput.js";
+
+try {
+	const headerFixed = new HeaderFixed({
+		HEADER: "header",
+		HEADER_FIXED: "header--fixed",
+	});
+
+	new BurgerMenu(
+		{
+			BURGER: "burger",
+			BURGER_OPEN: "burger--open",
+			HEADER_MENU: "header__menu",
+			HEADER_MENU_OPEN: "header__menu--open",
+			lABEL: {
+				OPEN: "Открыть меню",
+				CLOSE: "Закрыть меню",
+			},
+			PAGE_BODY: "page__body",
+			PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+			MENU_LINK: "menu__link",
+			BREAKPOINT: 768,
+			MAIN: "main",
+		},
+		headerFixed,
+	);
+
+	new Modal({
+		PAGE_BODY: "page__body",
+		PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
+	});
+
+	document.addEventListener("DOMContentLoaded", () => {
+		dropdownMenu();
+	});
+	productSlider();
+	buttonsSearch();
+} catch (error) {
+	console.error(error);
+}
